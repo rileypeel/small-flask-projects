b2971d4504d45bd6add866fb19a5cf840d87ef86
Riley Peel <rpeel4@outlook.com>
2020-06-15 15:07:13 -0700

    set up dockerfile for the application

--- crud-todo-list/app.py
+++ crud-todo-list/app.py
@@ -3,5 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-db.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 db = SQLAlchemy(app)
+
+
+@app.route('/')
+def hello():
+    return "hello buddy"
